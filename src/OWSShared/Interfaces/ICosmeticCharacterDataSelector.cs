d1682e4df407a749843dbbdd0aaec031762f2165
nickmezencev <barselon8@outlook.com>
2023-03-20 05:37:20 -0600

    Cosmetic Character Data Selector

---
+++ src/OWSShared/Interfaces/ICosmeticCharacterDataSelector.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OWSShared.Interfaces
+{
+    public class MyGameCustomCharacterDataSelector : ICustomCharacterDataSelector
+    {
+        public bool ShouldExportThisCustomCharacterDataField(string fieldName)
+        {
+            if (fieldName == "CosmeticCharacterData"
+                || fieldName == "Level"
+                || fieldName == "Class")
+            {
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
